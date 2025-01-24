f525b6762c6f14ca3aab37c970bbd26a0bac2384
MerVitz <amakaluvitalis202@gmail.com>
2025-01-24 07:17:06 +0300

    Area

--- main.js
+++ main.js
@@ -1,14 +1,12 @@
 import redisClient from './utils/redis';
 
 (async () => {
-  console.log(redisClient.isAlive()); // Expected: true
+    console.log(redisClient.isAlive());
+    console.log(await redisClient.get('myKey'));
+    await redisClient.set('myKey', 12, 5);
+    console.log(await redisClient.get('myKey'));
 
-  console.log(await redisClient.get('myKey')); // Expected: null (key doesn't exist yet)
-
-  await redisClient.set('myKey', 12, 5); // Set "myKey" with value "12" and 5-second expiration
-  console.log(await redisClient.get('myKey')); // Expected: 12 (key exists)
-
-  setTimeout(async () => {
-    console.log(await redisClient.get('myKey')); // Expected: null (key expired after 5 seconds)
-  }, 10000); // Wait 10 seconds before checking
+    setTimeout(async () => {
+        console.log(await redisClient.get('myKey'));
+    }, 1000*10)
 })();
