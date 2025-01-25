5a7137c8650c89f30a6773e54d76bd168b6069d0
MerVitz <amakaluvitalis202@gmail.com>
2025-01-25 14:09:59 +0300

    Fixed eslint

--- controllers/AppController.js
+++ controllers/AppController.js
@@ -1,28 +1,28 @@
-import dbClient from '../utils/db.js';
-import redisClient from '../utils/redis.js';
+import dbClient from '../utils/db';
+import redisClient from '../utils/redis';
 
 class AppController {
-    static async getStatus(req, res) {
-        try {
-            const redisStatus = redisClient.isAlive();
-            const dbStatus = dbClient.isAlive();
+  static async getStatus(req, res) {
+    try {
+      const redisStatus = redisClient.isAlive();
+      const dbStatus = dbClient.isAlive();
 
-            res.status(200).json({ redis: redisStatus, db: dbStatus });
-        } catch (error) {
-            res.status(500).json({ error: 'Error checking system status' });
-        }
+      res.status(200).json({ redis: redisStatus, db: dbStatus });
+    } catch (error) {
+      res.status(500).json({ error: 'Error checking system status' });
     }
+  }
 
-    static async getStats(req, res) {
-        try {
-            const usersCount = await dbClient.nbUsers();
-            const filesCount = await dbClient.nbFiles();
+  static async getStats(req, res) {
+    try {
+      const usersCount = await dbClient.nbUsers();
+      const filesCount = await dbClient.nbFiles();
 
-            res.status(200).json({ users: usersCount, files: filesCount });
-        } catch (error) {
-            res.status(500).json({ error: 'Error fetching statistics' });
-        }
+      res.status(200).json({ users: usersCount, files: filesCount });
+    } catch (error) {
+      res.status(500).json({ error: 'Error fetching statistics' });
     }
+  }
 }
 
 export default AppController;
