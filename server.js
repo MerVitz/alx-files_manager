55c0129e1d8fb894edf2a8bafef0bd398cc310b0
MerVitz <amakaluvitalis202@gmail.com>
2025-01-25 14:01:14 +0300

    Fixed eslint

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
 import express from 'express';
-import routes from './routes/index.js';
+import routes from './routes/index';
 
 const app = express();
 const PORT = process.env.PORT || 5000;
@@ -12,7 +12,7 @@ app.use('/', routes);
 
 // Start server
 app.listen(PORT, () => {
-    console.log(`Server running on port ${PORT}`);
+  console.log(`Server running on port ${PORT}`);
 });
 
 export default app;
